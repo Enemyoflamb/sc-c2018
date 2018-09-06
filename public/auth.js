41425f76bd430d770eb5756231559b4193a5c1c0
NovaSagittarii <thomas1210@gmail.com>
2018-09-06 16:57:37 -0700

    firebase + login input

---
+++ public/auth.js
@@ -0,0 +1,46 @@
+const DB = firebase.database();
+const AUTH = firebase.auth();
+var email = "";
+var password = "";
+
+function update(){
+	email = document.getElementById('email').value;
+	password = document.getElementById('password').value;
+}
+
+function register(){
+	update();
+	AUTH.createUserWithEmailAndPassword(email, password).then(function(){
+		// redirect to landing
+		alert("account created!");
+	}).catch(function(error){
+		switch(error.code){
+			case "auth/email-already-in-use":
+			case "auth/invalid-email":
+				// unhide "bad email" error
+				break;
+			case "auth/weak-password":
+				// unhide "weak password" error
+		}
+	});
+}
+function login(){
+	update();
+	AUTH.signInWithEmailAndPassword(email, password).then(function(){
+		// redirect to landing
+		alert("login successful!");
+
+	}).catch(function(error){
+		alert("login failed :(");
+		document.getElementById('password').value = "";
+	  // unhide error <p>
+	  // set password.value to ""
+	});
+}
+function exit(){
+	firebase.auth().signOut().then(function() {
+	  // Sign-out successful.
+	}).catch(function(error) {
+	  // An error happened.
+	});
+}
---
+++ public/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHs-5ONQJu-b54zPzSyWqlZvfq_JRnYRI",
+    authDomain: "smartchart-d879c.firebaseapp.com",
+    databaseURL: "https://smartchart-d879c.firebaseio.com",
+    projectId: "smartchart-d879c",
+    storageBucket: "smartchart-d879c.appspot.com",
+    messagingSenderId: "1093748540673"
+  };
+  firebase.initializeApp(config);
