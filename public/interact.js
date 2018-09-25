d6e8ca44233090c89f02bb33e6236c6f3626f8a3
Enemyoflamb <blobofrandomstuff@gmail.com>
2018-09-25 15:48:48 -0700

    to be FIXED

--- public/interact.js
+++ public/interact.js
@@ -1,3 +1,17 @@
+function show(){
+  var showelem= document.getElementsByClassName("hide")[0];
+  var movedRight = -40;
+  var show = setInterval(right, 5);
+  function right(){
+    if (movedRight >= 2){
+      clearInterval(show);
+    }else{
+      movedRight = movedRight + 0.1;
+      showelem.style.marginLeft = movedRight + "vw";
+    }
+  }
+}
+
 function hide(){
   var elem = document.getElementsByClassName("show")[0];
   var moved = -25;
@@ -8,6 +22,33 @@ function hide(){
     }else{
       moved = moved - 0.1;
       elem.style.marginLeft = moved + "vw";
+    }
+  }
+  show();
+}
+function showAddStudent(){
+  var elem = document.getElementsByClassName("show")[0]
+  var moved = -40;
+  var show = setInterval(right, 5);
+  function right(){
+    if (moved == -2){
+      clearInterval(show);
+    }else{
+      moved = moved + 0.1;
+      elem.style.marginLeft = moved + "vw";
+    }
+  }
+}
+function hideInfo(){
+  var showelem = document.getElementsByClassName("hide")[0];
+  var moveInfo = 2;
+  var goLeft = setInterval(infoLeft, 5);
+  function infoLeft(){
+    if (moveInfo == -40){
+      clearInterval(goLeft);
+    }else{
+      moveInfo = moveInfo - 0.1;
+      showelem.style.marginLeft = moveInfo + "vw";
     }
   }
 }
